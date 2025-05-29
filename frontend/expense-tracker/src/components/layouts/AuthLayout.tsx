31cdd1cf279c22c20430bb7df3351522425005e9
frako23 <frako23@gmail.com>
2025-05-29 13:25:40 -0400

    feat: implement login layout with AuthLayOut component

---
+++ frontend/expense-tracker/src/components/layouts/AuthLayout.tsx
@@ -0,0 +1,12 @@
+import React, { type ReactNode } from "react";
+
+const AuthLayOut = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+      <h2 className="text-lg font-medium text-black">Expense Tracker</h2>
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayOut;
